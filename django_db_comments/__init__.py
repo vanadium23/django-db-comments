87477e0d4872c73ea1069bfd72a20b501c9b106a
Christian Hartung <christian.hartung@olist.com>
2022-09-13 11:37:58 -0300

    Fix automatic detection warning for Django 3.2+

--- django_db_comments/__init__.py
+++ django_db_comments/__init__.py
@@ -1,3 +1,6 @@
+import django
+
 __version__ = "0.4.1"
 
-default_app_config = "django_db_comments.apps.DjangoDbCommentsConfig"
+if django.VERSION < (3, 2):
+    default_app_config = "django_db_comments.apps.DjangoDbCommentsConfig"
